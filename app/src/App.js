6e6afff868e0ff3b3ec62064eaf525ba53892e41
Stig Andreas Langeland <stg@hotmail.no>
2019-01-22 02:24:34 +0100

    Add styles to react app

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
 import axios from 'axios'
 import Sensor from './Components/Sensor'
 import NewSensor from './Components/NewSensor'
@@ -24,7 +23,6 @@ class App extends Component {
 
   setData() {
     axios.get("https://limitless-mesa-30279.herokuapp.com/current").then(res => {
-      console.log(res.data.data);
       this.setState({
         loading: false,
         data: res.data
@@ -36,42 +34,15 @@ class App extends Component {
     })
   }
 
-  addSensor = (name) => {
-
-    if (name === "" || name === undefined) {
-      return; 
-    }
-
-    const id = this.state.data ? this.state.data[this.state.data.length-1].id +1 : 1;
-    let temp = ((Math.random() * 30.0) - 10).toFixed(2);  
-    let humidity = Math.floor(Math.random()*100); 
-
-    let sensor = {
-      name: name,
-      id: id, 
-      temp: temp, 
-      humidity: humidity
-    }
-
-    let sensors = this.state.data; 
-    sensors.push(sensor); 
-
-    this.setState({
-      data: sensors
-    })
-  }
-
 
   render() {
-
-    console.log("KAKE:" + this.state.data); 
     const sensors = this.state.data; 
     const sensorList = sensors.length ? (
         <div className="row">
         {sensors.map((sensor, index) => {
             return (
               <React.Fragment key={index}>
-                  <Sensor sensor={sensor} loading={this.state.loading} key={index} />
+                  <Sensor sensor={sensor} sensor_class="normal" key={index} />
                   {(index+1) % 3 === 0 && index > 0 && !this.state.loading && <div className="w-100"></div>}
               </React.Fragment>
             )
@@ -83,14 +54,18 @@ class App extends Component {
     return (
 
       <div className="App">
+
+      <div id="header">
+            <p>Temperatur og Luftfuktighet</p>
+        </div>
+
         <div className="container">
           {this.state.loading ? (
           <div className="text-center">
-          <div className="spinner-border Top-margin" role="status">
-          </div>
+          <div className="spinner-border Top-margin" role="status"></div>
         </div>)
-        : (<p>{sensorList}</p>)}
-            <NewSensor addSensor={this.addSensor} />
+        : sensorList 
+        }
         </div>
       </div>
     );
